c1a6a3687397bf51cc14c797f5a93174d54e7e29
Ruan Hartman <st10453370@imconnect.edu.za>
2025-04-18 12:29:39 +0200

    5

---
+++ PROG6221_POE_PART1_ST10453370/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PROG6221_POE_PART1_ST10453370
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
